6053e2c5daa39d47977cd3c6166fd7aae90ddb8b
So5L <89294084+So5L@users.noreply.github.com>
2024-08-04 21:43:48 +0900

    dart tour fin.

--- main.dart
+++ main.dart
@@ -1,7 +1,22 @@
+class Strong {
+  final double strenghtLevel = 1500.99;
+}
+
+class QuickRunner {
+  void runQuick() {
+    print("ruuuuuuuuun!");
+  }
+}
+
+class Tall {
+  final double height = 1.00;
+}
+
+class Player with Strong, QuickRunner, Tall {}
+
 void main() {
-  String? name;
-  name ??= 'min';
-  name = null;
-  name ??= 'another';
-  print(name);
+  var player = Player(
+    team: Team.red,
+    name: 'min',
+  );
 }
